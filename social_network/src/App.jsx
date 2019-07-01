db8e3339e80510c24c2c3be25f37bd38e6b297b4
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-07-01 22:34:57 +0300

    feat: base components structure

--- social_network/src/App.jsx
+++ social_network/src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="app-wrapper" >
-      <header className="header" >
-        <img src="https://bcassetcdn.com/asset/logo/e33c12b4-1128-4129-b31f-2c31cb0878c7/logo?v=4&text=Logo+Text+Here" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content"></div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   )
 }
---
+++ social_network/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header" >
+            <img src="https://bcassetcdn.com/asset/logo/e33c12b4-1128-4129-b31f-2c31cb0878c7/logo?v=4&text=Logo+Text+Here" />
+        </header>
+    )
+}
+
+export default Header;
