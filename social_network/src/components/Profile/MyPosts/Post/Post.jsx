d310e5ce7655c48d2ceff33c30bbc3c05f9b714f
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-07-03 15:24:32 +0300

    refactor: add props to post

--- social_network/src/components/Profile/MyPosts/Post/Post.jsx
+++ social_network/src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './Post.module.css';
 
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={styles.item}>
             <img src='https://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/12/avatar-sequel.jpg?itok=B-LrB8Rq' />
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like {props.likeCount}</span>
             </div>
         </div>
     )
