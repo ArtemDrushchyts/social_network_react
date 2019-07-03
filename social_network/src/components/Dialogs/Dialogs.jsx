f2f84f2763754a9625ddac790ba8b3a9942c7166
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-07-03 21:02:49 +0300

    refactor: dialog module

--- social_network/src/components/Dialogs/Dialogs.jsx
+++ social_network/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import styles from './Dialogs.module.css'
+import styles from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
-const Dialogs = (props) => {
+const DialogItem = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={`${styles.dialog} ${styles.active}`}>
+            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        </div>
+    )
+};
+
+const Message = (props) => {
+    return (
+        <div className={styles.message}>
+            {props.message}
+        </div>
+    )
+};
+
+const Dialogs = () => {
+    return (
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <DialogItem name='Artem' id='1'/>
+                <DialogItem name='Senya' id='2'/>
+                <DialogItem name='Alesya' id='3'/>
+            </div>
+            <div className={styles.messages}>
+                <Message message='Hi'/>
+                <Message message='How are you?'/>
+                <Message message='Yooo'/>
+            </div>
         </div>
     )
 };
--- social_network/src/components/Navbar/Navbar.jsx
+++ social_network/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <div>
-                <a href='/profile'>Profile</a>
+            <div className={styles.item}>
+                <NavLink to='/profile' activeClassName={styles.activeLink}>Profile</NavLink>
             </div>
-            <div>
-                <a href='/dialogs'>Messages</a>
+            <div className={styles.item}>
+                <NavLink to='/dialogs' activeClassName={styles.activeLink}>Messages</NavLink>
             </div>
-            <div>
-                <a href='/news'>News</a>
+            <div className={styles.item}>
+                <NavLink to='/news' activeClassName={styles.activeLink}>News</NavLink>
             </div>
-            <div>
-                <a href='/music'>Music</a>
+            <div className={styles.item}>
+                <NavLink to='/music' activeClassName={styles.activeLink}>Music</NavLink>
             </div>
-            <div>
-                <a href='/settings'>Settings</a>
+            <div className={styles.item}>
+                <NavLink to='/settings' activeClassName={styles.activeLink}>Settings</NavLink>
             </div>
         </nav>
     )
-}
+};
 
 export default Navbar;
