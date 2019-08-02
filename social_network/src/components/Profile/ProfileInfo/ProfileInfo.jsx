8e9a9ff09b41b1907261942ffaeed86e54796011
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-08-02 22:43:40 +0300

    refactor: add contact description

--- social_network/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ social_network/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -13,8 +13,11 @@ const ProfileInfo = (props) => {
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'/>
             </div>
             <div className={styles.descriptionBlock}>
+                <h3>{props.profile.fullName}</h3>
                 <img src={props.profile.photos.large}/>
-                ava + description
+                <p>Обо мне: {props.profile.aboutMe}</p>
+                <p>В поиске работы: {props.profile.lookingForAJob === true ? 'Да' : 'Нет'} </p>
+                <p>Описание: {props.profile.lookingForAJobDescription}</p>
             </div>
         </div>
     )
