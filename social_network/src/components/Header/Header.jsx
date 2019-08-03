1b1024ba19d66413f1b80b58c6a38f8566cb0225
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-08-03 19:52:29 +0300

    feat: add login to app

--- social_network/src/components/Header/Header.jsx
+++ social_network/src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={styles.header} >
             <img src="https://bcassetcdn.com/asset/logo/e33c12b4-1128-4129-b31f-2c31cb0878c7/logo?v=4&text=Logo+Text+Here" />
+
+            <div className={styles.loginBlock}>
+                {props.isAuth ? props.login :
+                    <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     )
 };
